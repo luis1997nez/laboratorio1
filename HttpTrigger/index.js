6f96f9f21efcde4d3525a3fb191b6afd2f91a8c6
Luis Enrique Cutiz Dominguez <nez_gamer1997@hotmail.com>
2021-10-07 18:07:21 -0500

    regresion

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -2,12 +2,21 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Welcome, " + name
-        : "Pass a name in the query string or in the request body for a personalized response.";
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    //---
+    if(name) {
+
+        context.res = {
+            status: 200,
+            body: "Welcome, " + name
+        };
+
+    } else {
+
+        context.res = {
+            status: 400,
+            body: "Pass a name in the query string or in the request body"
+        };
+        
+    }
 }
